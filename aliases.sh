ae38f40086e7be51abb355724bde19edd3f5bb5f
Henry Majoros <majoros.henry@gmail.com>
2016-05-31 20:19:03 -0400

    first commit

---
+++ aliases.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+alias ..="cd .."
+alias ls="ls -G"
+alias cl="clear"
+
+# tmux aliases
+alias tmux="tmux -2"
+alias tmn="tmux -2 new -s"
+alias tma="tmux attach -t"
+alias tmls="tmux list-sessions"
+alias tmk="tmux kill-session -t"
+
