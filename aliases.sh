b774f895812da921a7e04ce4b734a51b968982f9
Henry Majoros <hmajoros@linkedin.com>
2020-11-18 08:39:30 -0800

    add vim and other aliases

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+alias v="vim"
 alias ..="cd .."
 alias ls="ls -G"
 alias cl="clear"
@@ -11,3 +12,17 @@ alias tma="tmux attach -t"
 alias tmls="tmux list-sessions"
 alias tmk="tmux kill-session -t"
 
+# turn newline separated output (like from sed / ag) into space separated output
+alias ws="tr '\n' ' '"
+
+# LinkedIn Aliases
+alias job="cd ~/repos/voyager-web_trunk/"
+alias bjob="cd ~/repos/backup/voyager-web_trunk/"
+alias dco="git review dcommit -r"
+alias rb="rbt post HEAD~1..HEAD"
+alias sub="git submit --async"
+alias lint="./.multiproduct/git-hooks/pre-create-review"
+alias tlint="./node_modules/.bin/ember-template-lint --config-path .template-lintrc.js"
+
+# Make ag ignore shit again
+alias ag="ag --path-to-ignore ~/.ignore"
